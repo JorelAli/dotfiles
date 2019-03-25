d4e3dcef0a7207ccee6e9d20a6e4c35fdfc43284
JorelAli <jorel.k.ali@gmail.com>
2019-03-25 21:49:40 +0000

    Document gitignore

---
+++ init.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.*;
+import java.io.*;
+import java.nio.*;
+import java.util.*;
+import java.awt.*;
+import java.lang.reflect.*;
+import java.util.stream.*;
+
+public void clear() { //literally prints out "Ctrl + L"
+	System.out.println("\033[H\033[2J");
+}
