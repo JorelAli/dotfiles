9e374304adf6594c7f5052be0570c086c33b3183
Jorel Ali <jorel.k.ali@gmail.com>
2019-06-07 06:09:35 +0100

    Adds ln function

--- nixprojects/math.nix
+++ nixprojects/math.nix
@@ -37,16 +37,22 @@ rec {
       then builtins.foldl' builtins.mul 1 (builtins.genList (val: x) power)
       else throw "Power must be an integer";
 
-  sqrt = x: sqrt' x 0.1 2 100;
+  sqrt = x: let
+    sqrt' = x: x0: root: iter: let
+      num = (pow x0 root) - x;
+      denom = x0 * root;
+      x1 = x0 - (num / denom);
+    in if iter == 0 
+      then x0 
+      else sqrt' x x1 root (iter - 1); 
+  in
+    sqrt' x 0.1 2 100;
 
-  sqrt' = x: x0: root: iter: let
-    num = (pow x0 root) - x;
-    denom = x0 * root;
-    x1 = x0 - (num / denom);
-  in if iter == 0 
-    then x0 
-    else sqrt' x x1 root (iter - 1);
-
-
-  
+  ln = x: let 
+    ln' = z: precision: builtins.genList (k:
+    let zs = (z - 1) / (z + 1.0); in
+      2 * (1.0 / (2 * k + 1)) * (pow zs (2 * k + 1))
+    ) precision; in
+    builtins.foldl' builtins.add 0 (ln' x 1000);
+    
 }
