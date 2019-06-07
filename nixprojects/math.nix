52df91aec811a1265f3a25cd5db13c064804ea71
Jorel Ali <jorel.k.ali@gmail.com>
2019-06-07 06:16:47 +0100

    Clean up builtins

--- nixprojects/math.nix
+++ nixprojects/math.nix
@@ -1,4 +1,8 @@
-rec {
+let inherit (builtins) 
+isInt foldl' genList 
+currentTime toString 
+div head fromJSON split
+add mul; in rec {
   abs = x: if x < 0 then x * -1 else x;
   
   # Floor' has this bug:
@@ -6,25 +10,25 @@ rec {
   # math.floor (-1.0) => -2
   floor' = x: 
     let toInt = str:
-      let may_be_int = builtins.fromJSON str; in
-      if builtins.isInt may_be_int
+      let may_be_int = fromJSON str; in
+      if isInt may_be_int
         then may_be_int
         else throw "Could not convert ${str} to int."; in 
-    if builtins.isInt x 
+    if isInt x 
       then x 
       else 
         let result = toInt 
-          (builtins.head (builtins.split "\\." (builtins.toString x))); 
+          (head (split "\\." (toString x))); 
         in if x < 0 then result - 1 else result;
   ceil = x: 1 + (floor' x);
 
-  mod = base: int: base - (int * (builtins.div base int));
+  mod = base: int: base - (int * (div base int));
 
   pi = 3.141592653589793;
   e = 2.718281828459045;
 
   # Random. Once.
-  pseudoRand = bound: mod (builtins.currentTime) bound;
+  pseudoRand = bound: mod (currentTime) bound;
   pseudoRandRange = min: max:
     if min >= max
       then throw "max must be greater than min"
@@ -33,8 +37,8 @@ rec {
   max = x: y: if x > y then x else y;
   min = x: y: if x > y then y else x;
   pow = x: power: 
-    if builtins.isInt power 
-      then builtins.foldl' builtins.mul 1 (builtins.genList (val: x) power)
+    if isInt power 
+      then foldl' mul 1 (genList (val: x) power)
       else throw "Power must be an integer";
 
   sqrt = x: let
@@ -49,10 +53,10 @@ rec {
     sqrt' x 0.1 2 100;
 
   ln = x: let 
-    ln' = z: precision: builtins.genList (k:
+    ln' = z: precision: genList (k:
     let zs = (z - 1) / (z + 1.0); in
       2 * (1.0 / (2 * k + 1)) * (pow zs (2 * k + 1))
     ) precision; in
-    builtins.foldl' builtins.add 0 (ln' x 1000);
+    foldl' add 0 (ln' x 1000);
     
 }
