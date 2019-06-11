e925d21874ff65f13df63f53cff880e3b3b6df21
Jorel Ali <jorel.k.ali@gmail.com>
2019-06-11 14:02:30 +0100

    Adds sZipWith

--- nixprojects/streams.nix
+++ nixprojects/streams.nix
@@ -1,10 +1,19 @@
 with builtins; rec {
 
+  # stream :: (a -> a) -> Stream
   stream = f: { stream = f; };
 
-  # On average, max size is just over 18000
+  # take :: a -> Stream -> [a]
   take = n: stream: 
     if n == 0 then []
     else (take (n - 1) stream) ++ [(stream.stream n)];
+
+  # sZipWith :: Stream -> Stream -> (a -> a -> a) -> Stream
+  #
+  # Example:
+  #   sZipWith (stream (n: n*n)) (stream (n: n)) (s1: s2: s1+s2)
+  #   => stream (n*n + n)
+  sZipWith = {stream} @ stream1: {stream} @ stream2: f:
+    { stream = n: f (stream1.stream n) (stream2.stream n); };
   
 }
