725deaa56fe162174b86406ef30a447100b5ec64
Jorel Ali <jorel.k.ali@gmail.com>
2019-09-18 00:35:17 +0100

    Fix sound with dunst

--- .config/dunst/notifsound.sh
+++ .config/dunst/notifsound.sh
@@ -1,2 +1,5 @@
-#!/bin/sh
+#!/run/current-system/sw/bin/bash
+#sudo cvlc ./notifsound.mp3 --play-and-exit
+#sudo -u jorel mpg123 ./notifsound.mp3
+#ding
 mpv /home/jorel/.config/dunst/notifsound.mp3
